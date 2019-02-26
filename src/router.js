0ffae799a3dd02b89cf7e7b6a32e92f99a9ba7b5
Aad Versteden <madnificent@gmail.com>
2019-02-27 00:38:48 +0100

    Steps towards a transioning menu
    The hero we have now should transition from a one-pager to a hero.
    Some of the necessary transitions that can make this happen have been
    put in place.

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route('documentation');
+});
 
 export default Router;
